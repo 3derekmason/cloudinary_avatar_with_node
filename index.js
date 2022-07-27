2cc805aa546973e602669d4b0fb0620fd9875811
Derek Mason <3derekmason@gmail.com>
2022-07-26 18:55:12 -0600

    transform image controller

--- index.js
+++ index.js
@@ -48,3 +48,26 @@ const getAssetInfo = async (publicId) => {
     console.error(error);
   }
 };
+
+//////////////////////////////////////////////////////////////
+// Creates an HTML image tag with a transformation that
+// results in a circular thumbnail crop of the image
+// focused on the faces, applying an outline of the
+// first color, and setting a background of the second color.
+//////////////////////////////////////////////////////////////
+const createImageTag = (publicId, ...colors) => {
+  // Set the effect color and background color
+  const [effectColor, backgroundColor] = colors;
+
+  // Create an image tag with transformations applied to the src URL
+  let imageTag = cloudinary.image(publicId, {
+    transformation: [
+      { width: 250, height: 250, gravity: "faces", crop: "thumb" },
+      { radius: "max" },
+      { effect: "outline:10", color: effectColor },
+      { background: backgroundColor },
+    ],
+  });
+
+  return imageTag;
+};
